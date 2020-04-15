ada1c52f9046684f590db9238b1d842982ebd3d3
nitmws <mwsteidl@mws.at>
2020-04-15 15:33:09 +0200

    information language edited, test images resized

--- _tool-package/embedIPTCpmd_windows.cmd
+++ _tool-package/embedIPTCpmd_windows.cmd
@@ -5,7 +5,7 @@ echo This script requires an installed exiftool program
 pause
 
 REM adding IPTC Photo Metadata Standard properties in the file myiptcpmd.json
-exiftool -v -r -overwrite_original -j=myiptcpmd.json .\images
+exiftool_windows -v -r -overwrite_original -j=myiptcpmd.json .\images
 
 echo DONE
 
